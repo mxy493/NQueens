023a93b0d3f0b3703f13e8c3e0767f76bf3c123f
mxy493 <mxy20493@gmail.com>
2019-12-30 11:16:20 +0800

    Update NQueens.h

--- NQueens.h
+++ NQueens.h
@@ -17,7 +17,7 @@ class NQueens
 public:
     NQueens();
     ~NQueens();
-    vector<vector<string>> solveNQueens(int n, int algorithm);
+    vector<vector<string>> solveNQueens(int n, int algorithm);//返回查找到的结果
     double getTime(){return time;}
 private:
     double time;
